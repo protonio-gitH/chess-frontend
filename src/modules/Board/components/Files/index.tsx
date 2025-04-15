6ae413926f1e36c43e26f7955642805d511987d7
protonio-gitH <blinovkirill321@gmail.com>
2025-04-15 23:52:08 +0300

    add _variables.scss in Board module and refactoring styles

--- src/modules/Board/components/Files/index.tsx
+++ src/modules/Board/components/Files/index.tsx
@@ -4,7 +4,7 @@ import { Files as FilesConstants } from '../../constants/Files';
 
 const Files: FC = () => {
 	return (
-		<ul className={styles['board__files-list']}>
+		<ul className={styles['files-list']}>
 			{Object.keys(FilesConstants).map(file => (
 				<li key={file}>{file}</li>
 			))}
