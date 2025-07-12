486c30708f2f10f252e23baf41c0d97a972316b5
protonio-gitH <blinovkirill321@gmail.com>
2025-07-13 00:39:07 +0300

    add react-router-dom

---
+++ src/app/Game/index.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import styles from './index.module.scss';
+import { Board, BoardComponent, Colors, useSelected } from '../../modules/Board';
+import { Move } from '../../modules/MoveHistory';
+import { MoveHistoryContainer } from '../../modules/MoveHistory';
+
+function Game() {
+	const [board, setBoard] = useState<Board>(new Board());
+	const [moves, setMoves] = useState<Record<Colors.WHITE | Colors.BLACK, Move[]>>({
+		[Colors.WHITE]: [],
+		[Colors.BLACK]: [],
+	});
+
+	useEffect(() => {
+		restart();
+	}, []);
+
+	useEffect(() => {
+		const moves = board.moveHistory.getMoves();
+		setMoves({
+			white: [...moves.movesWhite],
+			black: [...moves.movesBlack],
+		});
+	}, [board.move]);
+
+	function restart() {
+		const newBoard = new Board();
+		newBoard.initCells();
+		newBoard.initFigures();
+		setBoard(newBoard);
+	}
+
+	return (
+		<div className={styles.game}>
+			<div className={styles.gameContent}>
+				<BoardComponent board={board} setBoard={setBoard} />
+				<MoveHistoryContainer board={board} setBoard={setBoard} moves={moves} setMoves={setMoves} />
+			</div>
+		</div>
+	);
+}
+
+export default Game;
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,43 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import styles from './index.module.scss';
-import { Board, BoardComponent, Colors, useSelected } from '../modules/Board';
-import { MoveList } from '../modules/MoveHistory';
-import { Move } from '../modules/MoveHistory';
-import { MoveHistoryContainer } from '../modules/MoveHistory';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Game from './Game';
 
 function App() {
-	const [board, setBoard] = useState<Board>(new Board());
-	const [moves, setMoves] = useState<Record<Colors.WHITE | Colors.BLACK, Move[]>>({
-		[Colors.WHITE]: [],
-		[Colors.BLACK]: [],
-	});
-
-	useEffect(() => {
-		restart();
-	}, []);
-
-	useEffect(() => {
-		const moves = board.moveHistory.getMoves();
-		setMoves({
-			white: [...moves.movesWhite],
-			black: [...moves.movesBlack],
-		});
-	}, [board.move]);
-
-	function restart() {
-		const newBoard = new Board();
-		newBoard.initCells();
-		newBoard.initFigures();
-		setBoard(newBoard);
-	}
-
 	return (
-		<div className={styles.app}>
-			<div className={styles.playContent}>
-				<BoardComponent board={board} setBoard={setBoard} />
-				<MoveHistoryContainer board={board} setBoard={setBoard} moves={moves} setMoves={setMoves} />
-			</div>
-		</div>
+		<BrowserRouter>
+			<Routes>
+				{/* <Route path="/" element={<Home />} /> */}
+				<Route path="/game" element={<Game />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
