f35dfeba439440b88f0360421d920b0b0e598682
protonio-gitH <blinovkirill321@gmail.com>
2025-07-16 02:38:29 +0300

    last changes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Game from './Game';
+import Main from './Main';
+import Layout from '../components/Layout';
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				{/* <Route path="/" element={<Home />} /> */}
-				<Route path="/game" element={<Game />} />
+				<Route path="/" element={<Layout />}>
+					<Route index element={<Main />} />
+					<Route path="game" element={<Game />} />
+				</Route>
 			</Routes>
 		</BrowserRouter>
 	);
